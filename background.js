821d1662eec4716a491b08162fe664d0ccb35fff
Josh Wood <83368581+Bloodgood101@users.noreply.github.com>
2024-05-31 10:28:41 +0100

    Changed some formatting

--- background.js
+++ background.js
@@ -5,18 +5,18 @@ window.onload = function() {
 
 function startAnime() {
   //gets the anime the user entered and downloads it from a torrent website (to be converted in Vuze and streamed on VLC)
-  var getAnime = document.getElementById("anime");
+  let getAnime = document.getElementById("anime");
   const animeName = getAnime.value;
 
-  var divLine = document.getElementById("confirmation");
+  let divLine = document.getElementById("confirmation");
   const newP = document.createElement('p');
   newP.textContent = "You have started "+animeName; 
   divLine.appendChild(newP);
 
   //Replace the animeName spaces with + so it fits the format for 1337x.to
-  animeNameUrl = animeName.replace(/ /g, "+");
+  let animeNameUrl = animeName.replace(/ /g, "+");
   //Our torrent website
-  var url = 'https://1337x.to/search/'+animeNameUrl+'/1/';
+  let url = 'https://1337x.to/search/'+animeNameUrl+'/1/';
   fetch(url, {
     mode: 'no-cors'
   }) 
@@ -40,4 +40,4 @@ function startAnime() {
     })
     //error handling
     .catch(error => console.error('Error fetching webpage:', error));
-};
+}
